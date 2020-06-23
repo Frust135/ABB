4f625a3bce7d736a638b1e1a3217bdef256f8191
Claudio Henríquez <63313873+ClaudioHenriquezBerroeta@users.noreply.github.com>
2020-06-23 16:54:21 -0400

    Update AppABB.java

--- AppABB.java
+++ AppABB.java
@@ -4,9 +4,12 @@ class AppABB{
 		a.insertar(5);
 		a.insertar(2);
 		a.insertar(8);
-		System.out.println("raiz "+a.raiz.dato);
-		System.out.println("hijo izquierdo "+a.raiz.hIzq.dato);
-		System.out.println("hijo derecho "+a.raiz.hDer.dato);
+		a.insertar(1);
+		//System.out.println("raiz "+a.raiz.dato);
+		//System.out.println("hijo izquierdo "+a.raiz.hIzq.dato);
+		//System.out.println("hijo derecho "+a.raiz.hDer.dato);
+		//System.out.println("hijo izquierdo del hijo izquierdo "+a.raiz.hIzq.hIzq.dato);
+		a.preorden();
 
 
 
