01fbaa1764dd22066f80178743d31d1b8aab5bc5
Claudio Henríquez <63313873+ClaudioHenriquezBerroeta@users.noreply.github.com>
2020-06-23 16:54:24 -0400

    Update ABB.java

--- ABB.java
+++ ABB.java
@@ -5,16 +5,42 @@ class ABB{
 		raiz=null;
 	}
 
-	void insertar(int x){ //NodoABB insertar(int x, NodoABB raiz)
+	public void insertar(int x){ //NodoABB insertar(int x, NodoABB raiz)
 		if(raiz==null){
 			raiz=new NodoABB(x);
 		}else{
 			if(x<raiz.dato){
-				raiz.hIzq=new NodoABB(x);
+				raiz.hIzq=insertar(x,raiz.hIzq);
 			}else{
-				raiz.hDer=new NodoABB(x);
+				raiz.hDer=insertar(x,raiz.hDer);
 			}
 		}
 
+	}
+	private NodoABB insertar(int x, NodoABB raiz){
+		if(raiz==null){
+			raiz=new NodoABB(x);
+		}else{
+			if(x<raiz.dato){
+				raiz.hIzq=insertar(x,raiz.hIzq);
+			}else{
+				raiz.hDer=insertar(x,raiz.hDer);
+			}
+		}
+		return raiz;
+	}
+	public void preorden(){
+		if(raiz!=null){
+			System.out.println(raiz.dato);
+			preorden(raiz.hIzq);
+			preorden(raiz.hDer);
+		}
+	}
+	private void preorden(NodoABB raiz){
+		if(raiz!=null){
+			System.out.println(raiz.dato);
+			preorden(raiz.hIzq);
+			preorden(raiz.hDer);
+		}
 	}
 }
