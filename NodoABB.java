309c4f05ee7d2636fadd6250ec233515a1beec57
Claudio Henríquez <63313873+ClaudioHenriquezBerroeta@users.noreply.github.com>
2020-06-23 15:34:04 -0400

    update

--- NodoABB.java
+++ NodoABB.java
@@ -1,3 +1,15 @@
 class NodoABB{
+	int dato;
+	NodoABB hIzq;
+	NodoABB hDer;
 
+	NodoABB(){
+		this.hIzq=null;
+		this.hDer=null;
+	}
+	NodoABB(int dato){
+		this.dato = dato;
+		this.hIzq=null;
+		this.hDer=null;
+	}
 }
